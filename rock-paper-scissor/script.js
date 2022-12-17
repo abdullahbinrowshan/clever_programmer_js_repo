8ad24902d0dbcf23194842b5782f1e8fbd942bbe
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2022-12-17 14:58:40 +0600

    I know I repeated my code but I think it's enough for now
    I will fix it later

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,26 +1,60 @@
 const scores = document.getElementById('scores');
-// const whoVsWho = document.getElementById('WvsW');
-// const winner = document.getElementById('winner');
+const whoVsWho = document.getElementById('WvsW');
+const winner = document.getElementById('winner');
 
-// console.log(scores.innerText ,whoVsWho.innerText, winner.innerText);
 
+let score = 0
 const gameFunction = (human, computer) => {
-    // console.log(scores);
-    let score
+    console.log(score);
     if (human === computer) {
-        score = 0
         scores.innerText = score
-    }else if (human == 1 ) {
-        score = 2
-    }else {
-        score = 5
+        whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+        winner.innerText = `It's a Draw!`
+    } else {
+        if (human == 'Rock' && computer == 'Scissor') {
+            score++
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Win!`
+        } else if (human == 'Rock' && computer == 'Paper') {
+            score--
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Lose!`
+        } else if (human == 'Paper' && computer == 'Scissor') {
+            score--
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Lose!`
+        } else if (human == 'Paper' && computer == 'Rock') {
+            score++
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Win!`
+        } else if (human == 'Scissor' && computer == 'Rock') {
+            score--
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Lose!`
+        } else if (human == 'Scissor' && computer == 'Paper') {
+            score++
+            scores.innerText = score
+            whoVsWho.innerText = `👶 ${human} vs 🤖 ${computer}`
+            winner.innerText = `You Win!`
+        }
     }
     return score
 }
 
 const humanFunction = (value) => {
-    const gameArr = ['rock', 'paper', 'scissor'];
+    const gameArr = ['Rock', 'Paper', 'Scissor'];
     const randomNumber = Math.floor(Math.random() * gameArr.length)
-    console.log(gameFunction(value, randomNumber))
-    // console.log(randomNumber);
+    console.log(gameFunction(gameArr[value], gameArr[randomNumber]))
+}
+
+const resetGame = () => {
+    score = 0
+    scores.innerText = '';
+    whoVsWho.innerText = '';
+    winner.innerText = '';
 }
